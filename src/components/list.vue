147d5d1b5bc14c9de2df1d60ff6037a9f954bf59
Amanda111 <631551773@qq.com>
2016-11-30 23:58:16 +0800

    const

--- src/components/list.vue
+++ src/components/list.vue
@@ -8,7 +8,6 @@
     <el-table-column
       prop="date"
       label="日期"
-      sortable
       width="180">
     </el-table-column>
     <el-table-column
@@ -49,25 +48,29 @@
 			return {
 				category:"",
 				tableData: [{
-				      date: '2016-05-02',
-				      name: '王小虎',
-				      address: '上海市普陀区金沙江路 1518 弄',
-				      tag: '家'
+              title:'haha',
+              count:'4',
+              tag:['好玩','有意思'],
+              saver:'amanda',
+              update:'2016-07-11'
 				    }, {
-				      date: '2016-05-04',
-				      name: '王小虎',
-				      address: '上海市普陀区金沙江路 1517 弄',
-				      tag: '公司'
+              title:'haha',
+              count:'4',
+              tag:['好玩','有意思'],
+              saver:'amanda',
+              update:'2016-07-11'
 				    }, {
-				      date: '2016-05-01',
-				      name: '王小虎',
-				      address: '上海市普陀区金沙江路 1519 弄',
-				      tag: '家'
+              title:'haha',
+              count:'4',
+              tag:['好玩','有意思'],
+              saver:'amanda',
+              update:'2016-07-11'
 				    }, {
-				      date: '2016-05-03',
-				      name: '王小虎',
-				      address: '上海市普陀区金沙江路 1516 弄',
-				      tag: '公司'
+              title:'haha',
+              count:'4',
+              tag:['好玩','有意思'],
+              saver:'amanda',
+              update:'2016-07-11'
 				}]
 			}
 		},
